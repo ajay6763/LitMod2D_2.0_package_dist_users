1df79e86f399f41787df8479183b78eabc91eb29
ajay6763 <ajay6763@gmail.com>
2020-03-27 00:19:40 +0100

    manual update

--- GUI/make_dist.sh
+++
@@ -1,6 +0,0 @@
-pyinstaller --noconfirm --log-level=WARN \
-    --onefile --nowindow \
-    --hidden-import=traits \
-    --hidden-import=traitsui \
-    --upx-dir=/usr/local/share/ \
-    main.spec
